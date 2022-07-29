d75cb9a142c687c837aae567c314b2c64f6d839a
Cody Bumgardner <cody@uky.edu>
2022-07-29 12:05:48 -0400

    blam

--- MIL.py
+++ MIL.py
@@ -217,7 +217,7 @@ def infer_epoch(model, loader, epoch, args, max_tiles=None):
             #print('batch_data 0: ' + str(type(batch_data)))
             #print('batch_data 1: ' + str(type(batch_data[0])))
             #print('batch_data 2: ' + str(batch_data[0]['Metadata']))
-            #print('image: ' + str(batch_data['image']))
+            print('data: ' + str(batch_data))
             #['metadata', 'metadata_meta_dict', 'metadata_transforms']
             #print('patch_location: ' + str(batch_data['patch_location'].as_dict("metadata", output_type=np.ndarray).keys()))
             #print('patch_location2: ' + str(
