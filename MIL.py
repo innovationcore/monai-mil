67ab7a189b62ad298b919a202122fa4b7aa518f7
codybum <cbumgardner@gmail.com>
2022-07-30 17:00:05 -0400

    update metrics

--- MIL.py
+++ MIL.py
@@ -275,11 +275,9 @@ def infer_epoch(model, loader, epoch, args, max_tiles=None):
                     for i in range(int(np.ceil(data.shape[1] / float(max_tiles)))):
                         data_slice = data[:, i * max_tiles : (i + 1) * max_tiles]
 
-                        print(data_slice)
-                        print(data_slice.cpu().detach().numpy())
-                        print(data.shape[1])
-                        exit(0)
                         logits_slice = model(data_slice, no_head=True)
+                        print(logits_slice)
+                        exit(0)
                         logits.append(logits_slice)
 
                         if has_extra_outputs:
